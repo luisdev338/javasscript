33b83352d5931976eafd68c7f4fe25f36ab40775
luisdev338 <luisfemipejazz90@gmail.com>
2024-08-21 10:06:18 -0300

    java

---
+++ javascript/operacaomatematicas/js/script.js
@@ -0,0 +1,37 @@
+// cinco opção matematicas + - * / %
+
+//soma
+console.log(5+3)
+const a = 3;
+const b = 2;
+console.log(a+b);
+//subtração
+console.log(3-2);
+const c = 8;
+const d = 3;
+const e = c-d;
+console.log(e);
+
+//multiplicação
+console.log(4*2);
+const f = 2;
+const g = f*2;
+console.log(g);
+
+// divisão
+console.log(5/2);
+
+
+//modulo - resto da divisão
+console.log(5%2);
+const h = 10;
+const i = 5;
+console.log(h / i );
+console.log(h % i);
+
+//variaveis var e let
+//var para escopo global
+//let para escopo
+var numero1 = 5;
+numero1 = 3;
+console.log(numero1);
---
+++ javascript/variaveisinterpolacao/js/script.js
@@ -0,0 +1,61 @@
+// COMENTARIO DE UMA LINHA
+
+/* COMENTARIOS DE VARIAS LINHA*/
+
+//NUMBER- INTEIROS- PONTOS FLUTUANTES
+//EXIBINDO UM  NUMERO NA TELA
+console.log(2); 
+
+//exibido o tipo de dado na tela
+console.log(typeof 2.5);
+
+//tres tipos de variaveis-constante não pode ser alterado
+//declarando constante e imprimindo
+const meunumero = 10;
+console.log(meunumero);
+
+//erro alterar constante
+const numero = 5;
+//numero = 3;
+console.log(numero);
+
+//string igual nome, conjunto de caracteres. strings aspas simple e dpkas
+console.log('Estou aprendendo javascript');
+console.log("Continue a aprendizagem");
+
+//conatgem de strings
+const nome = "carlos cunha";
+console.log(nome.length);
+
+//strimg de numeros
+const valores = "589";
+console.log(valores);
+console.log(typeof valores);
+
+//pular uma linha da string
+console.log("vou pular a linha \n R$ 50,00");
+
+//espaçamentos na string
+console.log("espaçamento \t na string");
+
+//variaveis var e let
+//var para escopo global
+//let para escopo
+var numero1 = 5;
+numero1 = 3;
+console.log(numero1);
+
+let numero2 = 10;
+numero2 = 3;
+console.log(numero2);
+
+//interpolacao inserir valores de variaveis
+const sobrenome = "cunha"
+console.log(`Ola, ${sobrenome}`);
+
+const idade = 34
+console.log(`sua idade é ${idade} anos`);
+
+console.log(`seu sobrenome é ${sobrenome}, sua idade ${idade}`);
+
+
